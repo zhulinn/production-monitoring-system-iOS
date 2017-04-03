32f3ce77e6d8d6d5a60f1e13a86d82cd0a2c1600
Julian Chu <Julian@juliandemacbook-air.local>
2017-04-03 15:08:19 +0000

    Initial Commit

---
+++ Monitor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Monitor
+//
+//  Created by Julian Zhu on 4/3/17.
+//  Copyright © 2017 Lin Zhu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
